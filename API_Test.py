1677747573a3cd24a7f3ecf25c0966354d0a82b8
priyadarshi <priyadarshi.k09@gmail.com>
2023-10-29 18:03:00 +0800

    New commit

--- API_Test.py
+++ API_Test.py
@@ -3,7 +3,7 @@ import requests
 api_key = "21eaf0f3ddae403e94624fbcc6007664"
 
 url = 'https://newsapi.org/v2/top-headlines?' \
-       'country=us&sortBy=publishedAt' \
+       'country=us&sortBy=publishedAt&' \
        'apiKey=21eaf0f3ddae403e94624fbcc6007664'
 
 r = requests.get(url)
